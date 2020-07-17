18a7937ad61a122fdb67bc078c28f4ac0608b046
Adarsha Poudel <adarsha@ucsb.edu>
2020-07-16 19:01:04 -0700

    Initial Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/style.css";
+import Board from "./components/board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1 className="title">2048</h1>
+        <div className="scores">
+          <div className="score">
+            <p>SCORE</p>
+            <p>{0}</p>
+          </div>
+          <div className="best">
+            <p>BEST</p>
+            <p>{0}</p>
+          </div>
+        </div>
+      </div>
+      <Board />
     </div>
   );
 }
