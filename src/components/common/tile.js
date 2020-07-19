d595f55bde19669a7fcf3d8740503baa585fb346
Adarsha Poudel <adarsha@ucsb.edu>
2020-07-19 04:41:13 -0700

    trying to figure out the correct algorithm for the game logic + added changing color

--- src/components/common/tile.js
+++ src/components/common/tile.js
@@ -4,9 +4,9 @@ import "../../css/style.css";
 class Tile extends Component {
   render() {
     const mystyle = {
-      backgroundColor: `rgb(${255 - ((this.props.value * 10) % 255)},${
-        200 - ((this.props.value * 7) % 100)
-      },${150 - ((this.props.value * 5) % 60)})`,
+      backgroundColor: `rgb(${238 - ((this.props.value * 20) % 255)},${
+        225 - ((this.props.value * 10) % 100)
+      },${215 - ((this.props.value * 5) % 60)})`,
     };
     return (
       <div className="tile" value={this.props.value} style={mystyle}>
