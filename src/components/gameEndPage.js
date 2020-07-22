37289c835f5f5ac5bd02f6529b66c1132dfaf548
Adarsha Poudel <adarsha@ucsb.edu>
2020-07-21 20:44:41 -0700

    Updated GameEnd functionality

---
+++ src/components/gameEndPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class gameEndPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Game Ends</h1>
+      </div>
+    );
+  }
+}
+
+export default gameEndPage;
